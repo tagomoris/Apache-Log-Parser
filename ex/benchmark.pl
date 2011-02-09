d504cf1ebdc218a7068b7dd89d168eb1a49d803f
TAGOMORI Satoshi <tagomoris@livedoor.jp>
2011-02-09 15:03:30 +0900

    fix benchmark for present code

--- ex/benchmark.pl
+++ ex/benchmark.pl
@@ -1,63 +1,74 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
 use Benchmark qw/timethese cmpthese/;
 
-use Ax::ApacheLogParser;
+use Apache::Log::Parser;
 use ApacheLog::Parser qw/parse_line_to_hash/;
 
-my $logline = q{localhost.local - - [04/Oct/2007:12:34:56 +0900] "GET /apache_pb.gif HTTP/1.1" 200 2326 "http://www.dan.co.jp/" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US; rv:1.8.1.7) Gecko/20070914 Firefox/2.0.0.7" "localhost" "127.0.0.1201102081601001" "000-mobilehost.any" 1280};
+my $common = q{localhost.local - - [04/Oct/2007:12:34:56 +0900] "GET /index.html HTTP/1.1" 200 2326};
+my $combined = q{localhost.local - - [04/Oct/2007:12:34:56 +0900] "GET /index.html HTTP/1.1" 200 2326 "http://www.google.com/" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.84 Safari/534.13"};
+my $custom = q{localhost.local - - [04/Oct/2007:12:34:56 +0900] "GET /index.html HTTP/1.1" 200 2326 "http://www.google.com/" "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-US) AppleWebKit/534.13 (KHTML, like Gecko) Chrome/9.0.597.84 Safari/534.13" "localhost" "127.0.0.1201102081601001" "000-mobilehost.any" 1280};
 
-my (@logline) =
-    ( $logline =~
-          m!^(.*) (.*) (.*) (\[.*\]) (\".*\") (.*) (.*) (\".*\") (\".*\") (\".*\") (\".*\") (\".*\") (.*)! );
+my $default_fast = Apache::Log::Parser->new( fast => 1 );
+my $common_fast = Apache::Log::Parser->new( fast => ['common'] );
+my $custom_fast = Apache::Log::Parser->new( fast => [[qw(refer agent vhost usertrack mobileid request_duration)], 'combined', 'common'] );
 
-my $combined = join " ", ($1, $2, $3, $4, $5, $6, $7, $8, $9);
-
-my $tsv = join "\t", @logline;
+my $default_strict = Apache::Log::Parser->new( strict => 1 );
+my $common_strict = Apache::Log::Parser->new( strict => ['common'] );
 
 my @customized_fields = qw( rhost logname user datetime request status bytes refer agent vhost usertrack mobileid request_duration );
-my $parser_strict = Ax::ApacheLogParser->new( strict => [
-    ["\t", \@customized_fields, sub{my $x=shift;defined($x->{vhost}) and defined($x->{usertrack}) and defined($x->{mobileid})}],
+my $custom_strict = Apache::Log::Parser->new( strict => [
     [" ", \@customized_fields, sub{my $x=shift;defined($x->{vhost}) and defined($x->{usertrack}) and defined($x->{mobileid})}],
     'combined',
     'common',
     'vhost_common'
 ]);
-my $parser_fast = Ax::ApacheLogParser->new( fast => [[qw(refer agent vhost usertrack mobileid request_duration)], 'combined', 'common']);
-my $parser_combined = Ax::ApacheLogParser->new( fast => 1 );
 cmpthese(
     timethese(
         0,
         {
-            rx_optim => sub {
-                my (@l) =
-                    ( $logline =~
-                          m!^([^\s]*) ([^\s]*) ([^\s]*) \[([^: ]+):([^ ]+) ([-+0-9]+)\]\s+"(\w+) ([^\s]*) ([^\s]*)"\s+([^\s]*)\s+([^\s]*)\s+"([^"]*)"\s+"([^"]*)"\s+"([^"]*)"\s+"([^"]*)"\s+"([^"]*)"\s+(\d+)!
-                      );
+            common_regex => sub {
+                my @values = ( $common =~ m!^([^\s]*)\s+([^\s]*)\s+([^\s]*)\s+\[([^: ]+):([^ ]+) ([-+0-9]+)\]\s+"(\w+) ([^\s]*) ([^\s]*)"\s+([^\s]*)\s+([^\s]*)! );
             },
-            rx_half_optim => sub {
-                my (@l) =
-                    ( $logline =~
-                          m!^([^\s]*) ([^\s]*) ([^\s]*) \[([^: ]+):([^ ]+) ([-+0-9]+)\]\s+"(\w+) ([^\s]*) ([^\s]*)"\s+([^\s]*)\s+([^\s]*)(\s+"([^"]*)")?(\s+"([^"]*)")?(\s+"([^"]*)")?(\s+"([^"]*)")?(\s+"([^"]*)")?(\s+(\d+))?!
-                      );
+            default_fast => sub { my $log = $default_fast->parse($common); },
+            common_fast => sub { my $log = $common_fast->parse($common); },
+            custom_fast => sub { my $log = $custom_fast->parse($common); },
+            default_strict => sub { my $log = $default_strict->parse($common); },
+            common_strict => sub { my $log = $common_strict->parse($common); },
+            custom_strict => sub { my $log = $custom_strict->parse($common); },
+        }
+    )
+);
+
+cmpthese(
+    timethese(
+        0,
+        {
+            combined_regex => sub {
+                my @values = ( $combined =~ m!^([^\s]*)\s+([^\s]*)\s+([^\s]*)\s+\[([^: ]+):([^ ]+) ([-+0-9]+)\]\s+"(\w+) ([^\s]*) ([^\s]*)"\s+([^\s]*)\s+([^\s]*)\s+"([^"]*)"\s+"([^"]*)"! );
             },
-            # tsv_split => sub { my (@l) = split /\t/, $tsv },
-            # tsv_split_parse => sub { my (@l) = split /\t/, $tsv ;
-            #                         foreach my $l (@l) {
-            #                              if (substr($l, 0, 1) eq '"') {
-            #                                  substr($l, 1, length($l) - 2);
-            #                              }
-            #                          }
-            #                          my (@dates) = ($l[4] =~ m!([^: ]+):([^ ]+)\s([-+0-9]+)!);
-            #                          my (@reqs) = split(/ /, $l[5], 3);
-            #                      },
-            # parser_strict_space => sub { my $log = $parser_strict->parse($logline); },
-            # parser_strict_tsv => sub { my $log = $parser_strict->parse($tsv); },
-            parser_fast_space => sub { my $log = $parser_fast->parse($logline); },
-            # parser_fast_tsv => sub { my $log = $parser_fast->parse($tsv); },
-            parser_fast_combined => sub { my $log = $parser_combined->parse($combined); },
-            apachelog_parser => sub { my %hash = parse_line_to_hash($combined); },
+            apachelog_parser => sub { my %log = parse_line_to_hash($combined); },
+            default_fast => sub { my $log = $default_fast->parse($combined); },
+            custom_fast => sub { my $log = $custom_fast->parse($combined); },
+            default_strict => sub { my $log = $default_strict->parse($combined); },
+            custom_strict => sub { my $log = $custom_strict->parse($combined); },
+        }
+    )
+);
+
+cmpthese(
+    timethese(
+        0,
+        {
+            custom_regex => sub {
+                my @values = ( $custom =~ m!^([^\s]*)\s+([^\s]*)\s+([^\s]*)\s+\[([^: ]+):([^ ]+) ([-+0-9]+)\]\s+"(\w+) ([^\s]*) ([^\s]*)"\s+([^\s]*)\s+([^\s]*)\s+"([^"]*)"\s+"([^"]*)"\s+"([^"]*)"\s+"([^"]*)"\s+"([^"]*)"\s+(\d+)! );
+            },
+            custom_regex2 => sub {
+                my @values = ( $custom =~ m!^([^\s]*)\s+([^\s]*)\s+([^\s]*)\s+\[([^: ]+):([^ ]+) ([-+0-9]+)\]\s+"(\w+) ([^\s]*) ([^\s]*)"\s+([^\s]*)\s+([^\s]*)\s+"([^"]*)"\s+"([^"]*)"(\s+"([^"]*)")?(\s+"([^"]*)")?(\s+"([^"]*)")?(\s+(\d+))?! );
+            },
+            custom_fast => sub { my $log = $custom_fast->parse($custom); },
+            custom_strict => sub { my $log = $custom_strict->parse($custom); },
         }
     )
 );
